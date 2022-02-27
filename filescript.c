5d75aebac9c22c44f6b9a1893c8284f3e4cb39e4
Michael <mnoon@calpoly.edu>
2022-02-27 11:34:32 -0800

    update script to keep files in memory

--- filescript.c
+++ filescript.c
@@ -6,17 +6,22 @@
 #include <unistd.h>
 #include <time.h>
 
+#define CPYCT 250000
+
 
 int main(){
     FILE* incp = fopen("master.txt", "rb");
-    for(int i=0;i<100000;i++){
+    FILE* outcp[CPYCT];
+    //FILE** outcp = malloc(sizeof(FILE*) * 1019);
+    for(int i=0;i<CPYCT;i++){
         char outname[100];
         sprintf(outname, "Output-%d.txt", i);
-        FILE* outcp = fopen(outname, "wb");
+        outcp[i] = fopen(outname, "wb");
         char ch;
-        while(fread(&ch, sizeof(char), 1, incp)) fwrite(&ch, sizeof(char), 1, outcp);
-        fclose(outcp);
+        while(fread(&ch, sizeof(char), 1, incp)) fwrite(&ch, sizeof(char), 1, outcp[i]);
+        if(i>=1019) fclose(outcp[i-1019]);
         fseek(incp, 0, SEEK_SET);
     }
+    //free(outcp);
     fclose(incp);
 }
