e63b01035b030288eae1c011ab2754ecb2972011
Michael <mnoon@calpoly.edu>
2022-03-03 18:59:18 -0800

    update script to avoid file descriptor limit

--- filescript.c
+++ filescript.c
@@ -5,23 +5,32 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/stat.h>
 
 #define CPYCT 250000
+#define FSZ 4097
 
 
 int main(){
     FILE* incp = fopen("master.txt", "rb");
-    FILE* outcp[CPYCT];
-    //FILE** outcp = malloc(sizeof(FILE*) * 1019);
+    struct stat buf;
+    stat("master.txt", &buf);
+    int fsz = buf.st_size;
+    //FILE* outcp[CPYCT];
+    FILE* outcp;
+    char* data[CPYCT];
     for(int i=0;i<CPYCT;i++){
         char outname[100];
         sprintf(outname, "Output-%d.txt", i);
-        outcp[i] = fopen(outname, "wb");
-        char ch;
-        while(fread(&ch, sizeof(char), 1, incp)) fwrite(&ch, sizeof(char), 1, outcp[i]);
-        if(i>=1019) fclose(outcp[i-1019]);
+        outcp = fopen(outname, "wb");
+        data[i] = malloc(sizeof(char)*fsz);
+        fread(&data[i], sizeof(char), fsz, incp);
+        fwrite(&data[i], sizeof(char), fsz, outcp);
+        //char ch;
+        //while(fread(&ch, sizeof(char), 1, incp)) fwrite(&ch, sizeof(char), 1, outcp[i]);
+        //if(i>=1019) fclose(outcp[i-1019]);
+        fclose(outcp);
         fseek(incp, 0, SEEK_SET);
     }
-    //free(outcp);
     fclose(incp);
 }
